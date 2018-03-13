4ac754086c9fca6866419a9f7b15f1be5e1a75c2
Korede Aderele <kaderele@gmail.com>
2018-03-13 17:58:01 -0400

    Add a bunch of boilerplate, update Makefile

---
+++ assn3/openhash.py
@@ -0,0 +1,23 @@
+class OpenHash:
+    """
+    OpenHash-based implementation of DIctionary ADT
+    """
+
+    def __init__(self):
+        self.ht = {}
+
+    @staticmethod
+    def INSERT(x, H):
+        pass
+
+    @staticmethod
+    def DELETE(x, H):
+        pass
+
+    @staticmethod
+    def MEMBER(x, H):
+        pass
+    
+    @staticmethod
+    def MAKENULL(H):
+        pass
