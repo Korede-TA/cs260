2ff233fc8202b6c8de033938895ebfd87a5b9c50
Sree <kuttansree172@gmail.com>
2018-03-18 19:44:23 -0400

    updated open hash

--- assn3/openhash.py
+++ assn3/openhash.py
@@ -15,7 +15,8 @@ class OpenHash:
 				i+=1
 
 	def _getHash(self, key):
-		return ord(key[0]) % len(self.ht)
+		print(len(self.ht))
+		return ord(key[0])%len(self.ht)
 
 	# @staticmethod
 	def insert(self, x, h):
@@ -39,6 +40,12 @@ class OpenHash:
 				i += 1
 		print('Key not in HashTable')
 
+	def printAll(self):
+		for i in range(0, len(self.ht)):
+			hash = self.ht[i]
+			if len(hash)>0:
+				print("index is: "+str(i)+" key is: \""+hash[0]+"\" hash is: \""+hash[1]+"\"")
+
 
 	# @staticmethod
 	# def MEMBER(x, H):
@@ -48,19 +55,25 @@ class OpenHash:
 	# def MAKENULL(H):
 	#    pass
 
+
 def time(out):
 	pass
 
 if __name__ == "__main__":
-	dict = OpenHash(250)
+	dict = OpenHash(25)
 
 	print('Vals Instertion')
+	dict.printAll()
 	dict.insert('Key','Val')
+	dict.printAll()
 	dict.insert('other','other val')
+	dict.printAll()
 	print('\nThe values of \'key\' is :', dict['Key'])
 
 	print('Deleting \'key\' from dictonary')
+	dict.printAll()
 	dict.delete('Key')
+	dict.printAll()
 
 
 	try:
