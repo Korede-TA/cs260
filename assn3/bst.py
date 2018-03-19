4f89c94d4d965b906f63f026fc8edb1d0bf6e359
pankaj-rai <raipankaj739@gmail.com>
2018-03-18 21:22:15 -0400

    3 and 4

--- assn3/bst.py
+++ assn3/bst.py
@@ -1,31 +1,91 @@
-class Node:
-    def __init__(self, val=None, *children):
-        self.val = val
-        self.children = list(children)
+#!/usr/bin/python
+import sys
+import collections
+from random import shuffle
+from math import log
 
-class BST:
-    """
-    BST-based implementation of Dictionary ADT
-    """
 
-    def __init__(self):
-        self.ht = {}
+class node :
+	def __init__(self, element=None, lc=None, rc=None) :
+		self.element = element
+		self.lc  = lc
+		self.rc = rc
 
-    @staticmethod
-    def INSERT(x, H):
-        pass
+	def __str__(self) :
+		return str(self.element)
 
-    @staticmethod
-    def DELETE(x, H):
-        pass
+def MEMBER(x, A) :
+	if A is None :
+		return False
+	elif x == A.element :
+		return True
+	elif x < A.element :
+		return MEMBER(x, A.lc)
+	elif x > A.element :
+		return MEMBER(x, A.rc)
 
-    @staticmethod
-    def MEMBER(x, H):
-        pass
-    
-    @staticmethod
-    def MAKENULL(H):
-        pass
+def INSERT(x, A) :
+	if A is None :
+		A = node(x)
+		A.lc = None
+		A.rc = None
+	elif x < A.element :
+		if A.lc is not None :
+			INSERT(x, A.lc)
+		else :
+			A.lc = node(x)
+	elif x > A.element :
+		if A.rc is not None :
+			INSERT(x, A.rc)
+		else :
+			A.rc = node(x)
+	return A
 
-if __name__ == "__name__":
-    print("BST")
+def DELETEMIN(A) :
+	if A.lc is None :
+		A = A.rc	
+		return A.element
+	else :
+		return DELETEMIN(A.lc)
+
+def DELETE(x, A) :
+	if A is not None :
+		if x < A.element :
+			DELETE(x, A.lc)
+		elif x > A.element :
+			DELETE(x, A.rc)
+		elif A.lc is None and A.rc is None :
+			A = None	
+		elif A.lc is None :
+			A = A.rc
+		elif A.rc is None :
+			A = A.lc
+		else :
+			A.element = DELETEMIN(A.rc)
+
+def LOCATE(x, A, i=0) :
+	if A is None :
+		return None
+	elif x == A.element :
+		return i
+	elif x < A.element :
+		return LOCATE(x, A.lc, i+1)
+	elif x > A.element :
+		return LOCATE(x, A.rc, i+1)
+
+NUMLOOPS = 25
+lengthList = []
+avgList = []
+lengthSum = 0
+for i in range(1, NUMLOOPS+1) :
+	MAXVALUE = 10 * i
+	values = range(0, MAXVALUE)
+	shuffle(values)
+	BST = None
+	for e in values :
+		BST = INSERT(e, BST)
+	for e in values :
+		lengthSum += LOCATE(e, BST)
+	avgList.append( float(lengthSum)/float(MAXVALUE) )
+for i in range(NUMLOOPS) :
+	print (i+1)*10, "\t\t", log((i+1)*10,2), "\t\t", avgList[i]
---
+++ assn3/trie.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+import sys
+import collections
+
+class Node:
+	def __init__(self):
+		self.cargo = None
+		self.nxt = None
+		self.child = None
+
+	def __str__(self):
+		return str(self.cargo)
+
+class myList:
+	def __init__(self):
+		self.head = None
+		self.cur = None
+
+def MAKENULL() :
+	temp = myList()
+	return temp
+
+def FIRST(lst) :
+	return lst.head
+
+def END(lst) :
+	if lst.head is None and lst.cur is None:
+		return None
+	else :
+		temp = lst.head
+		while temp.nxt:
+			temp = temp.nxt
+		return temp
+
+def LOCATE(val, lst) :
+	count = 0
+	temp = FIRST(lst)
+	while temp :
+		if temp.cargo == val :
+			return count
+		else :
+			count += 1
+			temp = temp.nxt;
+	return -1
+
+def RETREIVE(pos, lst) :
+	temp = FIRST(lst)
+	for k in range(0, pos) :
+		if temp.nxt != None :
+			temp = temp.nxt
+	return temp.cargo
+
+def GETNODE(pos, lst) :
+	temp = FIRST(lst)
+	for k in range(0, pos) :
+		if temp.nxt != None :
+			temp = temp.nxt
+	return temp
+
+def NEXT(n) :
+	return n.nxt
+
+def PREVIOUS(n, lst) :
+	temp = FIRST(lst)
+	while temp :
+		if temp.nxt == n :
+			return temp
+		else :
+			temp = temp.nxt
+	return None
+
+def INSERT(val, pos, lst) :
+	n = Node()
+	n.cargo = val
+	if pos is None :
+		n.nxt = None
+		lst.head = n
+		lst.cur = lst.head
+		return
+	elif (pos == FIRST(lst) and FIRST(lst) != END(lst)) or (pos == 0):
+		n.nxt = lst.head
+		lst.head = n
+		lst.cur = lst.head
+		return
+	else :
+		if lst.head is None :
+			lst.head = n
+			lst.cur = n
+		else :
+			tmp = FIRST(lst)
+			while tmp and pos > 1 :
+				tmp = tmp.nxt
+				pos -= 1
+			n.nxt = tmp.nxt
+			tmp.nxt = n
+		return
+
+def DELETE(pos, lst) :
+	temp = FIRST(lst)
+	if pos == 0 :
+		lst.head = temp.nxt
+	elif pos == END(lst) :
+		if temp.nxt == None :
+			lst = MAKENULL()
+		else :
+			while (temp.nxt).nxt :
+				temp = temp.nxt
+			temp.nxt = None
+	else :
+		while pos - 1 > 0 :
+			temp = temp.nxt
+			pos -= 1
+		first = temp
+		second = temp.nxt
+		first.nxt = second.nxt
+	lst.cur = lst.head
+
+def printList(lst) :
+	temp = FIRST(lst)
+	if temp is None :
+		print "empty",
+	else :
+		while temp :
+			print temp,
+			temp = temp.nxt
+
+def ADD_CHILD(letter, lst) :
+	l = myList()
+	n = Node()
+	n.cargo = letter
+	lst.child = l
+	l.head = n
+	l.cur = l.head
+	return l
+
+def ADD_WORD(word, lst) :
+	global wc
+	L = lst
+	for letter in word :
+		if L is not None :
+			if LOCATE(letter, L) == -1 :
+				INSERT(letter, END(L), L)
+				for ltr in word[word.index(letter)+1:] :
+					t = ADD_CHILD(ltr, L)
+					L = t
+				ADD_CHILD('$', L)
+				wc += 1
+				return lst
+			else :
+				L = GETNODE(LOCATE(letter, L), L).child
+		else :
+			L = MAKENULL()
+			INSERT(letter, END(L), L)
+			L = END(L)
+			for ltr in word[word.index(letter)+1:] :
+				t = ADD_CHILD(ltr, L)
+				L = t
+			ADD_CHILD('$', L)
+			wc += 1
+			return lst
+	return lst
+
+Trie = MAKENULL()
+
+global wc
+wc = 0
+import sys
+with open("alice30.txt") as f:
+	for rawline in f :
+		cleanline = rawline.strip().split(' ')
+		for word in cleanline :
+			Trie = ADD_WORD(word.lower(), Trie)
